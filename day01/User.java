0c5d2cec321179e9a38ad05b939218799178f806
heart1999 <44794837+heart1999@users.noreply.github.com>
2021-01-31 07:00:37 -0800

    Update and rename 基础练习.java to User.java

---
+++ day01/User.java
@@ -0,0 +1,19 @@
+package com.donglijiedian.javase;
+
+public class User {
+	private int age;
+	
+	public void setAge(int a) {
+		//编写业务逻辑代码进行安全控制
+		if(a<0||a>150) {
+			System.out.println("对不起，您提供的年龄不合法");
+			return;
+		}
+		age=a;
+		
+	}
+	public int getAge() {
+		return age;
+	}
+	
+}
