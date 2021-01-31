7c512eb7c7d1aa73d75aa8a12a850c9a6577901e
heart1999 <44794837+heart1999@users.noreply.github.com>
2021-01-31 07:01:31 -0800

    Create UserTest.java

---
+++ day01/UserTest.java
@@ -0,0 +1,9 @@
+package com.ydj1999.javase;
+
+public class UserTest {
+	public static void main(String[] args) {
+		User user=new User();
+		user.setAge(100);
+		System.out.println(user.getAge());
+	}
+}
